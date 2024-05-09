476643836ee7acd5b93e0d08a445f21054490fb7
Arthur C <arthurdourado01@hotmail.com>
2024-05-09 13:10:19 -0300

    Post CRUD

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,6 @@
-import { INewUser } from "@/types";
+import { INewPost, INewUser } from "@/types";
 import { ID, Query } from "appwrite";
-import { account, appwriteConfig, avatars, databases } from "./config";
+import { account, appwriteConfig, avatars, databases, storage } from "./config";
 
 export async function createUserAccount(user: INewUser) {
     try {
@@ -48,7 +48,7 @@ export async function saveUserToDB(user: {
         return newUser
     } catch (error) {
         console.log(error);
-        
+
     }
 }
 
@@ -61,7 +61,6 @@ export async function signInAccount(user: { email: string, password: string }) {
         console.log(error);
     }
 }
-
 
 export async function getCurrentUser() {
     try {
@@ -81,4 +80,111 @@ export async function getCurrentUser() {
     } catch (error) {
         console.log(error);
     }
+}
+
+export async function signOutAccount() {
+    try {
+        const session = await account.deleteSession('current')
+
+        return session
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function createPost(post: INewPost) {
+    try {
+        // Upload image to storage
+        const uploadedFile = await uploadFile(post.file[0])
+
+        if (!uploadedFile) throw Error
+
+        // Get file url
+        const fileUrl = getFilePreview(uploadedFile.$id)
+
+        if (!fileUrl) {
+            deleteFile(uploadedFile.$id)
+            throw Error
+        }
+
+        // Convert tags in an array
+        const tags = post.tags?.replace(/ /g, '').split(',') || []
+
+        // Save post to database
+        const newPost = await databases.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.postCollectionId,
+            ID.unique(),
+            {
+                creator: post.userId,
+                caption: post.caption,
+                imageUrl: fileUrl,
+                imageId: uploadedFile.$id,
+                location: post.location,
+                tags: tags
+            }
+        )
+
+        if (!newPost) {
+            deleteFile(uploadedFile.$id)
+            throw Error
+        }
+
+        return newPost
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function uploadFile(file: File) {
+    try {
+        const uploadedFile = await storage.createFile(
+            appwriteConfig.storageId,
+            ID.unique(),
+            file
+        )
+
+        return uploadedFile
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function getFilePreview(fileId: string) {
+    try {
+        const fileUrl = storage.getFilePreview(
+            appwriteConfig.storageId,
+            fileId,
+            2000,
+            2000,
+            'top',
+            100
+        )
+
+        return fileUrl
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function deleteFile(fileId: string) {
+    try {
+        await storage.deleteFile(appwriteConfig.storageId, fileId)
+
+        return { status: 'ok' }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function getRecentPosts() {
+    const posts = await databases.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.postCollectionId,
+        [Query.orderDesc('$createdAt'), Query.limit(20)]
+    )
+
+    if (!posts) throw Error
+
+    return posts
 }
