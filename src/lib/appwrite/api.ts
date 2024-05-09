2bb9007a04d42b081f54a7567715cdb6301c1de5
Arthur C <arthurdourado01@hotmail.com>
2024-05-09 16:06:54 -0300

    get file preview bug

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -94,23 +94,21 @@ export async function signOutAccount() {
 
 export async function createPost(post: INewPost) {
     try {
-        // Upload image to storage
         const uploadedFile = await uploadFile(post.file[0])
 
         if (!uploadedFile) throw Error
 
         // Get file url
         const fileUrl = getFilePreview(uploadedFile.$id)
-
         if (!fileUrl) {
-            deleteFile(uploadedFile.$id)
+            await deleteFile(uploadedFile.$id)
             throw Error
         }
 
-        // Convert tags in an array
+        // Convert tags into array
         const tags = post.tags?.replace(/ /g, '').split(',') || []
 
-        // Save post to database
+        // Create post
         const newPost = await databases.createDocument(
             appwriteConfig.databaseId,
             appwriteConfig.postCollectionId,
@@ -126,7 +124,7 @@ export async function createPost(post: INewPost) {
         )
 
         if (!newPost) {
-            deleteFile(uploadedFile.$id)
+            await deleteFile(uploadedFile.$id)
             throw Error
         }
 
@@ -150,7 +148,7 @@ export async function uploadFile(file: File) {
     }
 }
 
-export async function getFilePreview(fileId: string) {
+export function getFilePreview(fileId: string) {
     try {
         const fileUrl = storage.getFilePreview(
             appwriteConfig.storageId,
@@ -160,6 +158,8 @@ export async function getFilePreview(fileId: string) {
             'top',
             100
         )
+
+        if (!fileUrl) throw Error
 
         return fileUrl
     } catch (error) {
@@ -178,13 +178,17 @@ export async function deleteFile(fileId: string) {
 }
 
 export async function getRecentPosts() {
-    const posts = await databases.listDocuments(
-        appwriteConfig.databaseId,
-        appwriteConfig.postCollectionId,
-        [Query.orderDesc('$createdAt'), Query.limit(20)]
-    )
+    try {
+        const posts = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.postCollectionId,
+            [Query.orderDesc("$createdAt"), Query.limit(20)]
+        );
 
-    if (!posts) throw Error
+        if (!posts) throw Error;
 
-    return posts
+        return posts;
+    } catch (error) {
+        console.log(error);
+    }
 }
